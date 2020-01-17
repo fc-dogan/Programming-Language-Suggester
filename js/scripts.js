e5cd146d1cf8857f12a947fba3412019200ee879
Fatma <fceyda@gmail.com>
2020-01-17 14:49:48 -0800

    recreate branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   
-  $("form").submit(function(event){
+  $("#blanks form").submit(function(event){
     var interestInput = parseInt($("input#interest").val());
     var importantInput = $("input#important").val();
     var companyInput = $("input#company").val();
@@ -10,22 +10,26 @@ $(document).ready(function(){
     var emailInput = $("input.email").val();
  
 
-  $(".name").text(nameInput);
-  $(".email").text(emailInput);
-
-  if (importantInput === "3" && companyInput === "1" && 5<interestInput+choiseInput+musicalInput<=8 || importantInput === "1" && companyInput === "3" && 5<interestInput+choiseInput+musicalInput<=8 ){
-    $("#c").slideDown();
-    $("#form-input").hide();
-  } else if (importantInput === "2" && companyInput === "2" && 3<interestInput+choiseInput+musicalInput<=5 || importantInput === "1" && companyInput === "3" && 3<interestInput+choiseInput+musicalInput<=5){
-    $("#javascript").show();
-    $("#form-input").hide();
-  } else if (importantInput === "1" && companyInput === "1" && interestInput+choiseInput+musicalInput<=3 || importantInput === "3" && companyInput === "1" && interestInput+choiseInput+musicalInput<=3){
-    $("#pyton").show();
-    $("#form-input").hide();
-  }
-
- event.preventDefault();
+  
 
   });
 
 });
+
+function suggestion(nameInput, emailInput, importantInput, companyInput, interestInput, choiseInput, musicalInput, event) {
+  $(".name").text(nameInput);
+  $(".email").text(emailInput);
+  if (importantInput === "3" && companyInput === "1" && 5 < interestInput + choiseInput + musicalInput <= 8 || importantInput === "1" && companyInput === "3" && 5 < interestInput + choiseInput + musicalInput <= 8) {
+    $("#c").slideDown();
+    $("#form-input").hide();
+  }
+  else if (importantInput === "2" && companyInput === "2" && 3 < interestInput + choiseInput + musicalInput <= 5 || importantInput === "1" && companyInput === "3" && 3 < interestInput + choiseInput + musicalInput <= 5) {
+    $("#javascript").show();
+    $("#form-input").hide();
+  }
+  else if (importantInput === "1" && companyInput === "1" && interestInput + choiseInput + musicalInput <= 3 || importantInput === "3" && companyInput === "1" && interestInput + choiseInput + musicalInput <= 3) {
+    $("#pyton").show();
+    $("#form-input").hide();
+  }
+  event.preventDefault();
+}
