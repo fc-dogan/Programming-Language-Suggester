eee0bc75ce09b46058a58f4491d6f56e4c84eac3
Fatma <fceyda@gmail.com>
2020-01-17 14:13:08 -0800

    create branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
   
   $("form").submit(function(event){
-    var interestInput = $("input#interest").val();
+    var interestInput = parseInt($("input#interest").val());
     var importantInput = $("input#important").val();
     var companyInput = $("input#company").val();
-    var choiseInput = $("input#choise").val();
-    var musicalInput = parseInt($("input:radio[name:musical]:checked").val());
-    var nameInput = $("input#name").val();
-    var emailInput = $("input#email").val();
-  });
+    var choiseInput =parseInt($("input#choise").val());
+    var musicalInput = parseInt($("input:radio[name=musical]:checked").val());
+    var nameInput = $("input.name").val();
+    var emailInput = $("input.email").val();
+ 
 
   $(".name").text(nameInput);
   $(".email").text(emailInput);
 
-  
+  if (importantInput === "3" && companyInput === "1" && 5<interestInput+choiseInput+musicalInput<=8 || importantInput === "1" && companyInput === "3" && 5<interestInput+choiseInput+musicalInput<=8 ){
+    $("#c").slideDown();
+    $("#form-input").hide();
+  } else if (importantInput === "2" && companyInput === "2" && 3<interestInput+choiseInput+musicalInput<=5 || importantInput === "1" && companyInput === "3" && 3<interestInput+choiseInput+musicalInput<=5){
+    $("#javascript").show();
+    $("#form-input").hide();
+  } else if (importantInput === "1" && companyInput === "1" && interestInput+choiseInput+musicalInput<=3 || importantInput === "3" && companyInput === "1" && interestInput+choiseInput+musicalInput<=3){
+    $("#pyton").show();
+    $("#form-input").hide();
+  }
 
+ event.preventDefault();
 
-
-
-  
+  });
 
 });
