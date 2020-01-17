9e2ca98abfd5294ebe451870df59aee19f253ba1
Fatma <fceyda@gmail.com>
2020-01-17 13:02:42 -0800

    git commit -m q

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  $("form").submit(function(event){
+    var interestInput = $("input#interest").val();
+    var importantInput = $("input#important").val();
+    var companyInput = $("input#company").val();
+    var choiseInput = $("input#choise").val();
+    var musicalInput = parseInt($("input:radio[name:musical]:checked").val());
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+  });
+
+});
